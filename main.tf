57136e47a62d6dd753e0b42bee30a24d2b1d9bc8
Anki Reddy <ankireddy@bitcot.com>
2020-07-01 18:16:59 +0530

    all

--- main.tf
+++ main.tf
@@ -31,14 +31,160 @@ module "bootstrap" {
 }
 
 # Build the VPC
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.1.0.0/16"
-  instance_tenancy     = "default"
+resource "aws_vpc" "myvpc" {
+    cidr_block = "${var.VPC}"
+
+    tags = {
+        Name = "${var.stack}-vpc"
+    }
+}
+resource "aws_subnet" "public" {
+    count = "${length(var.public_subnets_cidr)}"
+    cidr_block = "${element(var.public_subnets_cidr,count.index)}"
+
+    vpc_id = "${aws_vpc.myvpc.id}"
+    availability_zone = "${element(var.azs,count.index)}"
+
+
+    tags = {
+        "Name" = "${var.stack}-subnet-${count.index +1}"
+    }
+  
+}
+
+
+#privatesubnets
+
+
+resource "aws_subnet" "private" {
+    count = "${length(var.private_subnets_cidr)}"
+    cidr_block = "${element(var.private_subnets_cidr,count.index)}"
+
+    vpc_id = "${aws_vpc.myvpc.id}"
+    availability_zone = "${element(var.azs,count.index)}"
+
+
+    tags = {
+        "Name" = "${var.stack}-privatesubnet-${count.index +1}"
+    }
+  
+}
+
+#IGW
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = "${aws_vpc.myvpc.id}"
+
+    tags = {
+        "Name" = "${var.stack}-IGW"
+    }
+  
+}
+
+#public_routetable
+resource "aws_route_table" "publicrt" {
+    vpc_id = "${aws_vpc.myvpc.id}"
+
+     route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.igw.id}"
+    }
+
+    tags = {
+        "Name" = "${var.stack}-public rt-tf"
+    }
+}
+
+
+
+
+
+#public_rt_subnets_assosiation
+resource "aws_route_table_association" "subnet1assoc" {
+    count = "${length(var.public_subnets_cidr)}"
+    subnet_id       = "${element(aws_subnet.public.*.id, count.index)}"
+    route_table_id  = "${aws_route_table.publicrt.id}"
+  
+}
+
+#  nat gateway
+
+resource "aws_nat_gateway" "nat" {
+  subnet_id     = "${element(aws_subnet.private.*.id, count.index)}"
+  allocation_id = "${aws_eip.eip.id}"
 
   tags = {
-    Name      = "anki2"
-    Terraform = "true"
+    Name = "${var.stack}-nat_gateway"
   }
+}
+
+resource "aws_eip" "eip" {
+
+  vpc = true
+
+  tags = {
+    Name = "${var.stack}-nat-ip"
+  }
+}
+
+
+#private _route_tables
+
+resource "aws_route_table" "privatecrt" {
+  vpc_id = "${aws_vpc.myvpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_nat_gateway.nat.id}"
+  }
+
+  tags = {
+    Name = "${var.stack}-private"
+  }
+}
+
+
+#private_rt_subnets_assosiation
+resource "aws_route_table_association" "privatesubnet1assoc" {
+    count = "${length(var.private_subnets_cidr)}"
+    subnet_id       = "${element(aws_subnet.private.*.id, count.index)}"
+    route_table_id  = "${aws_route_table.privatecrt.id}"
+  
+}
+
+
+resource "aws_security_group" "my_sg" {
+    name            = "${var.stack}-"
+    description     = "${var.stack}-security group "
+    vpc_id          = "${aws_vpc.myvpc.id}"
+     ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["157.44.73.63/32"]
+    }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress{
+        cidr_blocks = ["0.0.0.0/0"]
+        protocol    = "-1"
+        from_port   = "0"
+        to_port     = "0"
+    }
+    tags = {
+        Name = "${var.stack}-security_group"
+    }
 }
 
 
