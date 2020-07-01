19c485c01f99e3c6e8580e55e40f0b74b6fddd15
Anki Reddy <ankireddy@bitcot.com>
2020-07-01 18:32:56 +0530

    all

--- variable.tf
+++ variable.tf
@@ -1,8 +1,9 @@
 variable "VPC" {
-    type = "string"
+    type = string
     #description = "Enter vpc cidr range (eg. 10.0.0.0/16 )"
     default = "10.0.0.0/16"
 }
+/*
 variable "public_subnets_cidr" {
     type = "list"
     #description = "Enter public_subnets_cidr type as list (eg: [10.0.1.0/24,10.0.3.0/24]  )"
@@ -43,7 +44,9 @@ variable "region" {
     
 }
 */
+/*
 variable stack {
   #description = "Enter name for tags"
   default = "anki"
 }
+*/
