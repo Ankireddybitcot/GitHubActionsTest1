823c2d6729af972769d82d0bdac1b8954ff6241e
Anki Reddy <ankireddy@bitcot.com>
2020-07-01 20:48:09 +0530

    all

--- variable.tf
+++ variable.tf
@@ -1,81 +1,48 @@
-/*variable "VPC" {
-    type = string
-    #description = "Enter vpc cidr range (eg. 10.0.0.0/16 )"
-    default = "10.0.0.0/16"
-}
 
-/*
-variable "public_subnets_cidr" {
-    type = list
-    #description = "Enter public_subnets_cidr type as list (eg: [10.0.1.0/24,10.0.3.0/24]  )"
-    default = ["10.0.1.0/24","10.0.3.0/24"]
-
-}
-variable "azs" {
-    type = list
-   # description = "Enter Availability_zone type as list (eg : [us-west-2a,us-west-2b])"
-   default = ["us-west-2a","us-west-2b"]
-  
-}
-variable stack {
-  #description = "Enter name for tags"
-  default = "anki"
-}
-variable "private_subnets_cidr" {
-    type = list
-    #description = "Enter private_subnets_cidr type as list (eg: [10.0.2.0/24,10.0.4.0/24] )"
-    default = ["10.0.2.0/24","10.0.4.0/24"]
-    
-  }
-  */
-
-
-variable "VPC" {
-    type = "string"
-    #description = "Enter vpc cidr range (eg. 10.0.0.0/16 )"
-    default = "10.0.0.0/16"
-}
-variable "public_subnets_cidr" {
-    type = "list"
-    #description = "Enter public_subnets_cidr type as list (eg: [10.0.1.0/24,10.0.3.0/24]  )"
-    default = ["10.0.1.0/24","10.0.3.0/24"]
-
-}
-
-variable "private_subnets_cidr" {
-    type = "list"
-    #description = "Enter private_subnets_cidr type as list (eg: [10.0.2.0/24,10.0.4.0/24] )"
-    default = ["10.0.2.0/24","10.0.4.0/24"]
-    
-  
-}
-
-variable "azs" {
-    type = "list"
-   # description = "Enter Availability_zone type as list (eg : [us-west-2a,us-west-2b])"
-    default = ["us-west-2a","us-west-2b"]
-  
-}
 /*
 variable "accesskey" {
     type = "string"
-    #description = "Enter access_key"
-    
+    default = "AKIAWBEP2BYJSWMCCME6"
 }
 
 variable "secretkey" {
    type = "string"
-   description = "Enter secret_key"
-   
+   default ="LlfHPILHlVbk7dQ59Wq9Ye0HZ9vdvR62KRf3+OX1"
 }
 
 variable "region" {
     type = "string"
-    description = "Enter region name (eg. us-west-2 , us-east-1 , etc)"
-    
+    default = "us-west-2"
 }
 */
-variable stack {
-  #description = "Enter name for tags"
-  default = "anki"
+
+variable "name" {
+    type = "string"
+    #description = "Enter name of your account"  
+    default = "anki"
 }
+variable "threshold" {
+    type = "string"
+    #description = "pass the percentage threshold to get alarms"  
+    default = "30"
+}
+
+variable "email" {
+    type = "string"
+    #description = "Enter email to get notification"
+    default = "ankireddy@bitcot.com"
+  
+}
+
+variable "limitamount" {
+    #description = "enter value of limit amount USD (eg. 1 , 2 , 50 ,100) "
+    default = "1"
+
+}
+/*
+variable "timeunit" {
+    type = "string"
+    description = "Enter time_unit (eg .  MONTHLY, QUARTERLY, ANNUALLY ) "
+  
+}
+*/
