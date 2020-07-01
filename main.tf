8285c6e4d9b4d68b6151f53d53e3a55f94c853da
Anki Reddy <ankireddy@bitcot.com>
2020-07-01 18:47:07 +0530

    all

--- main.tf
+++ main.tf
@@ -31,11 +31,7 @@ module "bootstrap" {
 }
 
 
-variable "VPC" {
-    type = string
-    #description = "Enter vpc cidr range (eg. 10.0.0.0/16 )"
-    default = "10.0.0.0/16"
-}
+
 
 # Build the VPC
 resource "aws_vpc" "myvpc" {
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "VPC" {
+    type = string
+    #description = "Enter vpc cidr range (eg. 10.0.0.0/16 )"
+    default = "10.0.0.0/16"
+}
