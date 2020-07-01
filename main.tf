59fe6a45df9703cd4299ed7dcaaa583aaa1e8709
Anki Reddy <ankireddy@bitcot.com>
2020-07-01 20:11:00 +0530

    all

--- main.tf
+++ main.tf
@@ -154,9 +154,9 @@ resource "aws_route_table" "privatecrt" {
 
 #private_rt_subnets_assosiation
 resource "aws_route_table_association" "privatesubnet1assoc" {
-    count = "${length(var.private_subnets_cidr)}"
-    subnet_id       = "${element(aws_subnet.private.*.id, count.index)}"
-    route_table_id  = "${aws_route_table.privatecrt.id}"
+    count = length(var.private_subnets_cidr)
+    subnet_id       = element(aws_subnet.private.*.id, count.index)
+    route_table_id  = aws_route_table.privatecrt.id
   
 }
 
