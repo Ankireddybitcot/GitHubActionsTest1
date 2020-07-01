a050cd2ff7f7a6c04888f80b2635ac9943019d4c
Anki Reddy <ankireddy@bitcot.com>
2020-07-01 12:22:45 +0530

    all

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_route_table" "route_table1" {
   }
 }
 
-# Build route table 2
+# Build route table 2 dccedfef
 resource "aws_route_table" "route_table2" {
   vpc_id = aws_vpc.vpc.id
 
